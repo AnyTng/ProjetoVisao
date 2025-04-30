9d3088c4bc92b9bceecbaf109128d21eeb119de8
AnyTng <goncalo.tierri@gmail.com>
2025-04-30 12:16:57 +0100

    Fixed encoding

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
 #include <chrono>
-#include <opencv2\opencv.hpp>
-#include <opencv2\core.hpp>
-#include <opencv2\highgui.hpp>
-#include <opencv2\videoio.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
 
 extern "C" {
 #include "vc.h"
@@ -34,7 +34,7 @@ void vc_timer(void) {
 
 
 int main(void) {
-	// V�deo
+	// Vídeo
 	char videofile[] = "..\\video2.mp4";
 	cv::VideoCapture capture;
 	struct
@@ -48,35 +48,35 @@ int main(void) {
 	std::string str;
 	int key = 0;
 
-	/* Leitura de v�deo de um ficheiro */
+	/* Leitura de vídeo de um ficheiro */
 	/* NOTA IMPORTANTE:
-	O ficheiro video.avi dever� estar localizado no mesmo direct�rio que o ficheiro de c�digo fonte.
+	O ficheiro video.avi deverá estar localizado no mesmo directório que o ficheiro de código fonte.
 	*/
 	capture.open(videofile);
 
-	/* Em alternativa, abrir captura de v�deo pela Webcam #0 */
+	/* Em alternativa, abrir captura de vídeo pela Webcam #0 */
 	//capture.open(0, cv::CAP_DSHOW); // Pode-se utilizar apenas capture.open(0);
 
-	/* Verifica se foi poss�vel abrir o ficheiro de v�deo */
+	/* Verifica se foi possível abrir o ficheiro de vídeo */
 	if (!capture.isOpened())
 	{
-		std::cerr << "Erro ao abrir o ficheiro de v�deo!\n";
+		std::cerr << "Erro ao abrir o ficheiro de vídeo!\n";
 		return 1;
 	}
 	else
 	{
-		std::cout << "Ficheiro de v�deo aberto com sucesso!\n";
+		std::cout << "Ficheiro de vídeo aberto com sucesso!\n";
 	}
 
-	/* N�mero total de frames no v�deo */
+	/* Número total de frames no vídeo */
 	video.ntotalframes = (int)capture.get(cv::CAP_PROP_FRAME_COUNT);
-	/* Frame rate do v�deo */
+	/* Frame rate do vídeo */
 	video.fps = (int)capture.get(cv::CAP_PROP_FPS);
-	/* Resolu��o do v�deo */
+	/* Resolução do vídeo */
 	video.width = (int)capture.get(cv::CAP_PROP_FRAME_WIDTH);
 	video.height = (int)capture.get(cv::CAP_PROP_FRAME_HEIGHT);
 
-	/* Cria uma janela para exibir o v�deo */
+	/* Cria uma janela para exibir o vídeo */
 	cv::namedWindow("VC - VIDEO", cv::WINDOW_AUTOSIZE);
 
 	/* Inicia o timer */
@@ -84,31 +84,31 @@ int main(void) {
 
 	cv::Mat frame;
 	while (key != 'q') {
-		/* Leitura de uma frame do v�deo */
+		/* Leitura de uma frame do vídeo */
 		capture.read(frame);
 
 		/* Verifica se conseguiu ler a frame */
 		if (frame.empty()) break;
 
-		/* N�mero da frame a processar */
+		/* Número da frame a processar */
 		video.nframe = (int)capture.get(cv::CAP_PROP_POS_FRAMES);
 
-		// Fa�a o seu c�digo aqui...
+		// Faça o seu código aqui...
 		/*
 		// Cria uma nova imagem IVC
 		IVC *image = vc_image_new(video.width, video.height, 3, 255);
 		// Copia dados de imagem da estrutura cv::Mat para uma estrutura IVC
 		memcpy(image->data, frame.data, video.width * video.height * 3);
-		// Executa uma fun��o da nossa biblioteca vc
+		// Executa uma função da nossa biblioteca vc
 		vc_rgb_get_green(image);
 		// Copia dados de imagem da estrutura IVC para uma estrutura cv::Mat
 		memcpy(frame.data, image->data, video.width * video.height * 3);
-		// Liberta a mem�ria da imagem IVC que havia sido criada
+		// Liberta a memória da imagem IVC que havia sido criada
 		vc_image_free(image);
 		*/
 		// +++++++++++++++++++++++++
 
-		/* Exemplo de inser��o texto na frame */
+		/* Exemplo de inserção de texto na frame */
 		str = std::string("RESOLUCAO: ").append(std::to_string(video.width)).append("x").append(std::to_string(video.height));
 		cv::putText(frame, str, cv::Point(20, 25), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 0, 0), 2);
 		cv::putText(frame, str, cv::Point(20, 25), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 255, 255), 1);
@@ -125,8 +125,8 @@ int main(void) {
 		/* Exibe a frame */
 		cv::imshow("VC - VIDEO", frame);
 
-		/* Sai da aplica��o, se o utilizador premir a tecla 'q' */
-		key = cv::waitKey(1000 / video.fps); // Espera 1 segundo (1000ms) dividido pelo n�mero de frames por segundo do v�deo
+		/* Sai da aplicação, se o utilizador premir a tecla 'q' */
+		key = cv::waitKey(1000 / video.fps); // Espera 1 segundo (1000ms) dividido pelo número de frames por segundo do vídeo
 	}
 
 	/* Para o timer e exibe o tempo decorrido */
